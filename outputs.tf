6d02a29a2f118474225edde75bcf2adc4e5f9272
seonbo.shim <seonbo.shim@bespinglobal.com>
2022-09-13 15:44:07 +0900

    outputs 추가

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-#output "cluster_name" {
-#  value = module.es.cluster_name
-#}
+output "workgroup_id" {
+  value = try(aws_athena_workgroup.this.id, "")
+}
+
+output "workgroup_name" {
+  value = try(aws_athena_workgroup.this.name, "")
+}
+
+output "workgroup_arn" {
+  value = try(aws_athena_workgroup.this.arn, "")
+}
